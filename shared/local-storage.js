0e6259fdce08c7a51334f1554e6cca199a98f9d1
vascabarkapa <vascabarkapa@gmail.com>
2025-03-31 19:56:52 +0200

    getActiveWebSocketUrl

---
+++ shared/local-storage.js
@@ -0,0 +1,18 @@
+const LocalStorage = {
+    getItem(key) {
+        return JSON.parse(localStorage.getItem(key));
+    },
+
+    setItem(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+
+    removeItem(key) {
+        localStorage.removeItem(key);
+    },
+
+    getActiveWebSocketUrl() {
+        const settings = this.getItem("wsSettings");
+        return settings?.selected || settings?.custom || "wss://node928.info:82";
+    }
+};
