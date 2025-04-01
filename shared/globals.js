dc575db69515da6216866a684ecf2e597ab6e9fc
vascabarkapa <vascabarkapa@gmail.com>
2025-04-01 14:56:43 +0200

    added ws servers to globals

---
+++ shared/globals.js
@@ -0,0 +1,7 @@
+const WebSocketServers = [
+    {value: "wss://node928.info:82", label: "Asia [node928.info]"},
+    {value: "wss://bitcoin-betting.org:82", label: "Europe West [bitcoin-betting.org]"},
+    {value: "wss://node82.sytes.net:82", label: "Europe East [node82.sytes.net]"},
+    {value: "wss://sa.bitcoin-betting.com:82", label: "South America [bitcoin-betting.com]"},
+    {value: "", label: "Choose Custom"}
+];
