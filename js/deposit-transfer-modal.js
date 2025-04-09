ef58ec806f2f5f69b36c67d137ab8d896e20d170
vascabarkapa <vascabarkapa@gmail.com>
2025-04-09 22:24:10 +0200

    deposit-transfer-modal

---
+++ js/deposit-transfer-modal.js
@@ -0,0 +1,21 @@
+function loadDepositTransferModal() {
+    fetch('./components/shared/deposit-transfer-modal.html')
+        .then(res => res.text())
+        .then(html => {
+            document.body.insertAdjacentHTML('beforeend', html);
+
+            const depositBtn = document.getElementById("openDepositModalBtn");
+            if (depositBtn) {
+                depositBtn.addEventListener("click", () => {
+                    const modal = new bootstrap.Modal(document.getElementById("depositModal"));
+                    modal.show();
+                });
+            }
+
+            const user = LocalStorage.getItem("walletUser");
+            const userId = LocalStorage.getItem("userID");
+            if (user && userId) {
+                depositBtn?.classList.remove("d-none");
+            }
+        });
+}
